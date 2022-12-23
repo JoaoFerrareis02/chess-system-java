27fcfe5acb1f59c37a462ef149ca68ee93b91497
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2022-12-23 11:17:44 -0300

    Fist class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,36 @@
+package boardgame;
+
+public class Position {
+
+	private int row;
+	private int collum;
+
+	public Position(int row, int collum) {
+		this.row = row;
+		this.collum = collum;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCollum() {
+		return collum;
+	}
+
+	public void setCollum(int collum) {
+		this.collum = collum;
+	}
+
+	@Override
+	public String toString() {
+		return row +", "+collum;
+	}
+	
+	
+
+}
