440c943f6a7e723552b0221f087c746e4c4d2b4d
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2022-12-23 13:39:45 -0300

    Little improviment in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,9 +28,25 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('b',6,new Rook(board, Color.WHITE));
-		placeNewPiece('e',8,new King(board, Color.WHITE));
-		placeNewPiece('e',1,new King(board, Color.BLACK));
+		placeNewPiece('a',2,new Rook(board, Color.WHITE));
+		placeNewPiece('b',2,new Rook(board, Color.WHITE));
+		placeNewPiece('c',2,new Rook(board, Color.WHITE));
+		placeNewPiece('d',2,new Rook(board, Color.WHITE));
+		placeNewPiece('e',2,new Rook(board, Color.WHITE));
+		placeNewPiece('f',2,new Rook(board, Color.WHITE));
+		placeNewPiece('g',2,new Rook(board, Color.WHITE));
+		placeNewPiece('h',2,new Rook(board, Color.WHITE));
+		placeNewPiece('e',1,new King(board, Color.WHITE));
+		
+		placeNewPiece('a',7,new Rook(board, Color.BLACK));
+		placeNewPiece('b',7,new Rook(board, Color.BLACK));
+		placeNewPiece('c',7,new Rook(board, Color.BLACK));
+		placeNewPiece('d',7,new Rook(board, Color.BLACK));
+		placeNewPiece('e',7,new Rook(board, Color.BLACK));
+		placeNewPiece('f',7,new Rook(board, Color.BLACK));
+		placeNewPiece('g',7,new Rook(board, Color.BLACK));
+		placeNewPiece('h',7,new Rook(board, Color.BLACK));
+		placeNewPiece('e',8,new King(board, Color.BLACK));
 	}
 
 }
