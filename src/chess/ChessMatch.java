9eae8778c47b44c4d89eed259053eeec1ca41487
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2022-12-25 16:48:58 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
@@ -51,24 +54,13 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('a',2,new Rook(board, Color.WHITE));
-		placeNewPiece('b',2,new Rook(board, Color.WHITE));
-		placeNewPiece('c',2,new Rook(board, Color.WHITE));
-		placeNewPiece('d',2,new Rook(board, Color.WHITE));
-		placeNewPiece('e',2,new Rook(board, Color.WHITE));
-		placeNewPiece('f',2,new Rook(board, Color.WHITE));
-		placeNewPiece('g',2,new Rook(board, Color.WHITE));
-		placeNewPiece('h',2,new Rook(board, Color.WHITE));
+		
+		placeNewPiece('a',1,new Rook(board, Color.WHITE));
+		placeNewPiece('h',1,new Rook(board, Color.WHITE));
 		placeNewPiece('e',1,new King(board, Color.WHITE));
 		
-		placeNewPiece('a',7,new Rook(board, Color.BLACK));
-		placeNewPiece('b',7,new Rook(board, Color.BLACK));
-		placeNewPiece('c',7,new Rook(board, Color.BLACK));
-		placeNewPiece('d',7,new Rook(board, Color.BLACK));
-		placeNewPiece('e',7,new Rook(board, Color.BLACK));
-		placeNewPiece('f',7,new Rook(board, Color.BLACK));
-		placeNewPiece('g',7,new Rook(board, Color.BLACK));
-		placeNewPiece('h',7,new Rook(board, Color.BLACK));
+		placeNewPiece('a',8,new Rook(board, Color.BLACK));
+		placeNewPiece('h',8,new Rook(board, Color.BLACK));
 		placeNewPiece('e',8,new King(board, Color.BLACK));
 	}
 
